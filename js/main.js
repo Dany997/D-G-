f73c9820a48817027c3633c61aeef77cf2b21802
Dhuzio <dany97@o2.pl>
2023-10-05 16:27:09 +0200

    add header2

--- js/main.js
+++ js/main.js
@@ -2,15 +2,8 @@ const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year ');
 const handleNav = () => {
-
-
-
-
-    
-    
-navBtn.classList.toggle('is-active');
-navMobile.classList.toggle('nav-mobile--active');
-
+	navBtn.classList.toggle('is-active');
+	navMobile.classList.toggle('nav-mobile--active');
 };
 
 navBtn.addEventListener('click', handleNav);
