91e1526530676617c94e7c299b86c5eab662c124
Dhuzio <dany97@o2.pl>
2023-10-10 21:04:02 +0200

    add contact, fixed bug with nav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
+const allNavItems = document.querySelectorAll(`.nav__link`);
 const footerYear = document.querySelector('.footer__year ');
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
+
+	// solving bug with navigation
+
+	allNavItems.forEach((item) =>
+		item.addEventListener('click', () =>
+			navMobile.classList.remove('nav-mobile--active')
+		)
+	);
+	allNavItems.forEach((item) =>
+		item.addEventListener('click', () => navBtn.classList.remove('is-active'))
+	);
 };
 
 navBtn.addEventListener('click', handleNav);
